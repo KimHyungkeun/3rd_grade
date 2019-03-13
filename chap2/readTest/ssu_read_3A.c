06c5c89490a7a97529cc189a28c1d83d9e967d4b
KimHyunggeun <fnfn9087@gmail.com>
2019-03-13 23:43:27 +0900

    Modified0313

--- chap2/readTest/ssu_read_3A.c
+++ chap2/readTest/ssu_read_3A.c
@@ -16,7 +16,7 @@ void ssu_runtime (struct timeval* begin_t, struct timeval* end_t) {
         }
 
         end_t -> tv_usec -= begin_t -> tv_usec;
-        printf("Runtime : %ld:%06ld(sec:usec)\n",end_t -> tv_sec, end_t -> tv_usec);
+        printf("A_Runtime : %ld:%06ld(sec:usec)\n",end_t -> tv_sec, end_t -> tv_usec);
 
 }
 
