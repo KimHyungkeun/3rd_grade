2cae1210bff554b77a7264686710e5665f21e855
KimHyunggeun <fnfn9087@gmail.com>
2019-03-19 21:25:44 +0900

    Modified0319

--- chap2/writeTest/ssu_write_2.c
+++ chap2/writeTest/ssu_write_2.c
@@ -4,9 +4,10 @@
 #include <fcntl.h>
 #include <sys/time.h>
 
-#define S_MODE 0644
-#define BUFFER_SIZE 1024
+
 #define SECOND_TO_MICRO 1000000
+#define BUFFER_SIZE 1024
+#define S_MODE 0644
 
 void ssu_runtime (struct timeval* begin_t, struct timeval* end_t) {
 
@@ -22,41 +23,37 @@ void ssu_runtime (struct timeval* begin_t, struct timeval* end_t) {
 
 }
 
-
-
-
 int main(int argc, char *argv[])
 {
 	struct timeval begin_t, end_t;
 	char buf[BUFFER_SIZE];
-	int fd1, fd2;
-	int length;
-
+	int fd1, fd2, length;
+	
 	gettimeofday(&begin_t, NULL);
-
-	if (argc != 3) {
-		fprintf(stderr,"Usage : %s filein fileout\n",argv[0]);
+	if(argc != 3) {
+		fprintf(stderr,"Usage : %s filein fileout", argv[0]);
 		gettimeofday(&end_t,NULL);
-                ssu_runtime(&begin_t, &end_t);
+        	ssu_runtime(&begin_t, &end_t);
 		exit(1);
 	}
 
-	if ((fd1 = open(argv[1], O_RDONLY)) < 0) {
+	if((fd1 = open(argv[1], O_RDONLY)) < 0) {
 		fprintf(stderr, "open error for %s\n",argv[1]);
 		gettimeofday(&end_t,NULL);
-                ssu_runtime(&begin_t, &end_t);
+        	ssu_runtime(&begin_t, &end_t);
 		exit(1);
 	}
 
-	if((fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,S_MODE)) < 0){
-		fprintf(stderr, "open error for %s\n", argv[2]);
+	if((fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_MODE)) < 0) {
+		fprintf(stderr, "open error for %s\n",argv[2]);
 		gettimeofday(&end_t,NULL);
-                ssu_runtime(&begin_t, &end_t);
+        	ssu_runtime(&begin_t, &end_t);
 		exit(1);
 	}
 
-	while((length = write(fd1, buf, BUFFER_SIZE)) > 0)
+	while((length = read(fd1, buf, BUFFER_SIZE)) > 0) {
 		write(fd2, buf, length);
+	}
 
 
 	gettimeofday(&end_t,NULL);
