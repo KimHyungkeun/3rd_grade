13b46dde872a2ad0fd4e014f4def4ec45c821d82
KimHyunggeun <fnfn9087@gmail.com>
2019-04-01 00:18:44 +0900

    Modified0401

--- ssu_score/ssu_score.c
+++ ssu_score/ssu_score.c
@@ -1,66 +1,65 @@
 #include "ssu_score.h"
 #include "ssu_time.h"
 
-  
 int main(int argc, char *argv[])
 {
    int opt;
-   int qname_flag = 0;
-   int error_flag = 0;
-   int print_flag = 0;
-   int help_flag = 0;
-   char t_filename[20]; //filename by using t option
-   char e_filename[20];	//filename by using e option
+   int qname_flag = 0; //t 옵션
+   int error_flag = 0; //e 옵션
+   int print_flag = 0; //p 옵션
+   int help_flag = 0; // h 옵션
+   char t_filename[20]; //t 옵션에 들어갈 파일이름
+   char e_filename[20];	//e 옵션에 들어갈 파일이름
    char directory_path_ans[20] = "\0"; //ANS_DIR의 경로	   
    char directory_path_std[20] = "\0"; //STD_DIR의 경로
    gettimeofday(&begin_t, NULL);
 
-   if(argc < 3) {
+   if(argc < 3) { //인자가 3개 미만이면 종료
 	fprintf(stderr,"Usage : %s STD_DIR ANS_DIR [option]\n",argv[0]);
 	gettimeofday(&end_t,NULL);
-    ssu_runtime(&begin_t, &end_t);
+    	ssu_runtime(&begin_t, &end_t);
 	exit(1);
 	}
 
    strcpy(directory_path_std, argv[1]);
-   strcat(directory_path_std, "/");
+   strcat(directory_path_std, "/"); // "ANS_DIR/" 형태로 만든다
    strcpy(directory_path_ans, argv[2]); 
-   strcat(directory_path_ans, "/");
+   strcat(directory_path_ans, "/"); // "STD_DIR/" 형태로 만든다
 
-   while ((opt = getopt(argc, argv, "phe:t: ")) != -1) {
+   while ((opt = getopt(argc, argv, "phe:t: ")) != -1) { //옵션은 p,h,e,t 4종류가 있다
     switch(opt) {
 
-        case 'h' :
+        case 'h' : //help 옵션
             help_flag = 1;
             break;
          
-        case 't' :
+        case 't' : //lpthread 컴파일 옵션
             qname_flag = 1;
             for(int i = 0 ; i < argc ; i++) {
                 if(strcmp(argv[i], "-t") == 0) {
                     strcpy(t_filename, argv[i+1]);
-                    memcpy(t_filename, optarg, strlen(optarg));
+                    memcpy(t_filename, optarg, strlen(optarg)); //t 옵션 다음의 file에 대해 다룬다
                     break;
                 }
             }
             break;
         
-        case 'e' :
+        case 'e' : //error 폴더 생성 옵션
             error_flag = 1;
             for(int i = 0 ; i < argc ; i++) {
                 if(strcmp(argv[i], "-e") == 0) {
-                    strcpy(e_filename, argv[i+1]);
-                    memcpy(e_filename, optarg, strlen(optarg));
+                    strcpy(e_filename, argv[i+1]); 
+                    memcpy(e_filename, optarg, strlen(optarg)); // error파일을 담을 폴더명을 지정한다 
                     break;
                 }
             }
             break;
 
-        case 'p' :
+        case 'p' : //각 학생들의 score와 전체 평균을 출력하는 옵션이다.
             print_flag = 1;
             break;
 
-        case '?':
+        case '?': //파일 인자를 필요로 하는 옵션에서 유효하지 않은 값을 입력하면 프로그램을 종료시킨다.
             if(optopt == 't') {
                 printf("option -t requires [QNAME]\n");
                 gettimeofday(&end_t,NULL);
@@ -76,22 +75,8 @@ int main(int argc, char *argv[])
     
     }
    }
-   printf("%d %d %d %d\n", help_flag, qname_flag, error_flag, print_flag);
 
-   /*for(int i = 0 ; i < argc ; i++) {
-                if(strcmp(argv[i], "-t") == 0) {
-                    printf("index : %d option : %s\n", i, argv[i]);
-                    break;
-                }
-            }
-    for(int i = 0 ; i < argc ; i++) {
-                if(strcmp(argv[i], "-e") == 0) {
-                    printf("index : %d option : %s\n", i, argv[i]);
-                    break;
-                }
-            }*/
-
-   if(help_flag) {
+   if(help_flag) { //help 옵션을 작동시키면 사용법을 보여주고 프로그램을 종료시킨다.
 	fprintf(stderr,"Usage : %s STD_DIR ANS_DIR [option]\n-e <DIRNAME>   print error on 'DIRNAME/ID/qname_error.txt' file\n-t <QNAME>   compile QNAME.c with -lpthread option\n-h  print usage\n-p  print student's score and total average\n",argv[0]);
 	gettimeofday(&end_t,NULL);
     ssu_runtime(&begin_t, &end_t);
@@ -100,49 +85,49 @@ int main(int argc, char *argv[])
 
     
 
-    ssu_score_table_create(directory_path_ans);
-    score_table_create_proto(directory_path_std, directory_path_ans); //make prototpye of scoretable
-    program_autocompile(directory_path_std, directory_path_ans, qname_flag, error_flag, t_filename, e_filename); //program auto complile
-    program_problem_check(directory_path_std, directory_path_ans); //program problem compare
-    blank_problem_check(directory_path_std, directory_path_ans); //빈칸 채우기 문제 	
-    score_table_create(directory_path_std, directory_path_ans, print_flag);
+    ssu_score_table_create(directory_path_ans); //ssu_score.csv 파일을 만드는 함수
+    score_table_create_proto(directory_path_std, directory_path_ans); //score.csv를 만드는 함수
+    program_autocompile(directory_path_std, directory_path_ans, qname_flag, error_flag, t_filename, e_filename); //프로그래밍 문제 자동 컴파일
+    program_problem_check(directory_path_std, directory_path_ans); //프로그래밍 문제 채점
+    blank_problem_check(directory_path_std, directory_path_ans); //빈칸 채우기 문제 채점 	
+    score_table_create(directory_path_std, directory_path_ans, print_flag); //채점 후의 최종 score.csv를 생성
     
 
 	gettimeofday(&end_t,NULL);
-    ssu_runtime(&begin_t, &end_t);
+    ssu_runtime(&begin_t, &end_t); //프로그램 수행시간 측정
 	exit(0);
 
 }
 
 void program_autocompile(char* directory_path_std, char* directory_path_ans, int qname_flag, int error_flag, char* t_filename, char* e_filename){
     struct  dirent **namelist, **stdnamelist;
-    int     count, except = 0;
-    int     std_count=0;
+    int     count, except = 0; //count : scandir로 측정한 폴더갯수
+    int     std_count=0; //stddir 폴더 갯수
     int     err_dir_count = 0;
-    int     idx;
-    int     stdoutfile_fd, error_fd, fd_backup, fd_backup2;
-    int not_c;
-    int ptr_count;
-    char root_add_filename[10] = "\0"; 
-    char token_divide[30] = "\0";
-    char exe_filename[30] = "\0";
-    char stdout_filename[30] = "\0";
-    char txt_filename[30] = "\0";
-    char exe_syntax[30] = "\0";
-    char gcc_syntax[30] = "\0";
-    char run_syntax[30] = "\0";
-    char stdoutfile_name[30] = "\0";
-    char errorfile_name[30] = "\0";
-    char killall_command[30] = "\0";
-    char remove_error_directory[30] = "\0";
-    char make_error_directory[30] = "\0";
-    char move_error_file_syntax[50] = "\0";
-    char *ptr;
+    int     idx; //index로 쓸 변수
+    int     stdoutfile_fd, error_fd, fd_backup, fd_backup2; //stdoutfile과 error file을 만들기 위한 파일 디스크립터
+    int not_c; //c파일이 아님을 구별하기 위한 변수
+    int ptr_count; //포인터 변수 에서 토큰 기준으로 몇번 점프 했는지 카운팅
+    char root_add_filename[10] = "\0";  // "/" + "filename" = "/filename" 형태로 만듦
+    char token_divide[30] = "\0"; 
+    char exe_filename[30] = "\0"; //실행 파일 이름
+    char stdout_filename[30] = "\0"; //stdout 파일 이름
+    char txt_filename[30] = "\0"; //txt 파일 이름
+    char exe_syntax[30] = "\0"; //.exe 구문으로 만듦
+    char gcc_syntax[30] = "\0"; //gcc ... 구문으로 만듦
+    char run_syntax[30] = "\0"; //exe를 실행하기 위한 구문을 만듦
+    char stdoutfile_name[30] = "\0"; //stdout 파일 이름
+    char errorfile_name[30] = "\0"; //에러파일 이름
+    char killall_command[30] = "\0"; //동작중인 프로세스를 kill하기 위한 구문
+    char remove_error_directory[30] = "\0"; //error 디렉터리 제거
+    char make_error_directory[30] = "\0"; //error 디렉터리 생성
+    char move_error_file_syntax[50] = "\0";//error파일을 error 디렉터리로 움직이기위한 변수
+    char *ptr; // 토큰 분할 시 사용하는 임시 포인터 변수
     int compile_count = 0;
 
    
     
-    // ANS_DIR auto compile part
+    // ANS_DIR 자동 컴파일 부분
     if((count = scandir(directory_path_ans, &namelist, NULL, alphasort)) == -1) {
         fprintf(stderr, "%s Directory Scan Error: %s\n", directory_path_ans, strerror(errno));
         exit(1);
@@ -150,65 +135,62 @@ void program_autocompile(char* directory_path_std, char* directory_path_ans, int
     
     for(idx = 0; idx < count; idx++) {
         if(strcmp(namelist[idx] -> d_name,".") == 0 || strcmp(namelist[idx] -> d_name,"..") == 0)
-            continue;
+            continue; // .이나 ..이 발견되면 생략한다
         
         if(strcmp(namelist[idx] -> d_name, "score_table.csv") == 0 || strpbrk(namelist[idx] -> d_name,"-") != NULL)
-            continue;
+            continue; // score_table.csv이나 txt 문제들은 생략한다
 
         else {
         strcpy(dir_path_backup_ans, directory_path_ans);
-        strcat(directory_path_ans, namelist[idx] -> d_name);
+        strcat(directory_path_ans, namelist[idx] -> d_name); //"ANS_DIR/" 형태로 돌아옴
         strcat(directory_path_ans, "/");
-        strcpy(sub_path_ans_backup, directory_path_ans); // ANS_DIR/SUBDIR
+        strcpy(sub_path_ans_backup, directory_path_ans); // "ANS_DIR/SUBDIR"형태로 돌아옴
 
         sub_ans = opendir(directory_path_ans);
         while( entry_sub_ans = readdir(sub_ans)){
-            //compile_count++;
-            //printf("complie count : %d\n", compile_count);
-            //printf("%s\n", entry_sub_ans -> d_name);
-            if(strcmp(entry_sub_ans -> d_name,".") == 0 || strcmp(entry_sub_ans-> d_name,"..") == 0)
+
+            if(strcmp(entry_sub_ans -> d_name,".") == 0 || strcmp(entry_sub_ans-> d_name,"..") == 0)//.이나 ..은 생략
             continue;
 
             sprintf(exe_filename,"%s%s",namelist[idx] -> d_name,".exe");
-            if(strcmp(exe_filename,entry_sub_ans -> d_name) == 0)
+            if(strcmp(exe_filename,entry_sub_ans -> d_name) == 0) // ANS_DIR/SUB_DIR/*.exe 형태
                 continue;
             sprintf(stdout_filename,"%s%s",namelist[idx] -> d_name,".stdout");
-            if(strcmp(stdout_filename,entry_sub_ans -> d_name) == 0)
+            if(strcmp(stdout_filename,entry_sub_ans -> d_name) == 0) // ANS_DIR/SUB_DIR/*.stdout 형태
                 continue;
             
             else {
             strcat(directory_path_ans, namelist[idx] -> d_name);
     
             sprintf(exe_syntax, "%s%s" , sub_path_ans_backup, namelist[idx] -> d_name);
-            printf("exe_syntax %s\n", exe_syntax);
+            printf("exe_syntax %s\n", exe_syntax); // ANS_DIR/SUB_DIR/*.exe 형태
 
             sprintf(stdoutfile_name, "%s%s", exe_syntax, ".stdout");
-            printf("stdoutfile_name %s\n", stdoutfile_name);
+            printf("stdoutfile_name %s\n", stdoutfile_name); // ANS_DIR/SUB_DIR/*.stdout 형태
             
             
             if( (stdoutfile_fd = open(stdoutfile_name, O_RDWR | O_CREAT | O_TRUNC, 0640)) < 0) {
                     fprintf(stderr, "open error for %s\n", stdoutfile_name);
                     exit(1);
             }
-                fd_backup = dup(1);
+                fd_backup = dup(1); //표준출력 디스크립터 백업
               
                 sprintf(gcc_syntax, "%s %s%s %s %s %s%s", "gcc" , directory_path_ans,".c", "-lpthread","-o", exe_syntax,".exe");
-                system(gcc_syntax);
+                system(gcc_syntax); //gcc 로 컴파일 실시
 
-                //printf("%s\n", gcc_syntax);
-                dup2(stdoutfile_fd, 1);
+                dup2(stdoutfile_fd, 1); //stdout 파일에 컴파일 내용 담음
 
                 
                 first = time(NULL);
                 sprintf(run_syntax, "%s%s%s", "./",exe_syntax, ".exe");
-                system(run_syntax);
+                system(run_syntax); //프로그램 실행
                 second = time(NULL);
 
-                dup2(fd_backup, 1);
+                dup2(fd_backup, 1); //백업한 디스크립터 복원
                 printf("wait time is %f seconds\n\n", difftime(second, first));
                 close(stdoutfile_fd); 
 
-             strcpy(directory_path_ans, sub_path_ans_backup );
+             strcpy(directory_path_ans, sub_path_ans_backup ); //다시 ANS_DIR/ 형태로 되돌아간다
             }
             
         }
@@ -220,12 +202,12 @@ void program_autocompile(char* directory_path_std, char* directory_path_ans, int
     }
    
     for(idx = 0; idx < count; idx++) {
-        free(namelist[idx]);
+        free(namelist[idx]); //scandir 사용후 할당했던 메모리를 다시 반환한다
     }
 
     free(namelist);
 
-    // STD_DIR auto compile part
+    // STD_DIR 자동 컴파일 부문 
     
     if((count = scandir(directory_path_std, &namelist, NULL, alphasort)) == -1) {
         fprintf(stderr, "%s Directory Scan Error: %s\n", directory_path_std, strerror(errno));
@@ -234,34 +216,34 @@ void program_autocompile(char* directory_path_std, char* directory_path_ans, int
 
     for(idx = 0; idx < count; idx++) {
         if(strcmp(namelist[idx] -> d_name,".") == 0 || strcmp(namelist[idx] -> d_name,"..") == 0)
-            continue;
+            continue; //. 이나 ..을 만나면 생략하고 다음 디렉터리로 넘어간다
 
         else {
         strcpy(dir_path_backup_std, directory_path_std);
-        strcat(directory_path_std, namelist[idx] -> d_name);
+        strcat(directory_path_std, namelist[idx] -> d_name); // STD_DIR/ 형태
         strcat(directory_path_std, "/");
-        strcpy(sub_path_std_backup, directory_path_std); // STD_DIR/SUBDIR
+        strcpy(sub_path_std_backup, directory_path_std); // STD_DIR/SUBDIR 형태
 
         sub_std = opendir(directory_path_std);
         while( entry_sub_std = readdir(sub_std)){
             
             if(strcmp(entry_sub_std -> d_name,".") == 0 || strcmp(entry_sub_std-> d_name,"..") == 0)
-            continue;
+            continue; //.이나 ..을 만나면 생략하고 다음 파일을 읽음
 
         
             if(strpbrk(entry_sub_std -> d_name, "-") != NULL)
-                continue;
+                continue; //txt파일을 만나면 생략하고 다음 파일을 읽음
 
             for(int i=0 ; i < strlen(entry_sub_std -> d_name) ; i++) {
                 
                 if(entry_sub_std -> d_name [i] == 'e' || entry_sub_std -> d_name [i]== 's') {
-                    not_c = 1;
+                    not_c = 1; //exe나 stdout을 생략하기 위한 조건문
                 }
 
             }
             
             if(not_c == 1) {
-                not_c = 0;
+                not_c = 0; //만약 exe나 stdout파일이면 다음 파일로 넘어간다
                 continue;
             } 
                 
@@ -272,13 +254,13 @@ void program_autocompile(char* directory_path_std, char* directory_path_ans, int
             for(int i=0;i<strlen(directory_path_std);i++)
             {
                     if(directory_path_std[i] == 'c')
-                    directory_path_std[i] = '\0';
+                    directory_path_std[i] = '\0'; 
             }
 
             for(int i=0;i<strlen(directory_path_std);i++)
             {
                     if(directory_path_std[i] == '.')
-                    directory_path_std[i] = '\0';
+                    directory_path_std[i] = '\0'; //.stdout이나 .exe 라는 이름을 붙이기위해 .c라는 이름을 뗀다
             }
 
           
@@ -287,18 +269,17 @@ void program_autocompile(char* directory_path_std, char* directory_path_ans, int
             sprintf(exe_syntax, "%s" , directory_path_std);
             printf("exe_syntax %s\n", exe_syntax);
 
-            sprintf(stdoutfile_name, "%s%s", exe_syntax, ".stdout");
+            sprintf(stdoutfile_name, "%s%s", exe_syntax, ".stdout"); // *.exe 형태
             printf("stdoutfile_name %s\n", stdoutfile_name);
 
-            sprintf(errorfile_name, "%s%s", exe_syntax, "_error.txt");
-            printf("errorfile_name %s\n", errorfile_name);
+            sprintf(errorfile_name, "%s%s", exe_syntax, "_error.txt"); // *.stdout 형태
+            printf("errorfile_name %s\n\n", errorfile_name);
 
             
-            ptr = strstr(directory_path_std,"/12");
+            ptr = strstr(directory_path_std,"/12"); // "/12"라는 패턴을 문자열에서 찾는다.
             if(ptr == NULL)
-                ptr = "null";
+                ptr = "null"; //못찾으면 null이라는 문자열 대입
             sprintf(root_add_filename, "%s%s", "/", t_filename);
-            //printf("strcmp : %d\n", strcmp(ptr, root_add_filename));
             
             
             if( (stdoutfile_fd = open(stdoutfile_name, O_RDWR | O_CREAT | O_TRUNC, 0755)) < 0 ) {
@@ -331,14 +312,11 @@ void program_autocompile(char* directory_path_std, char* directory_path_ans, int
                     sprintf(run_syntax, "%s%s%s %s", "./",exe_syntax, ".exe","&");
 
 
-                    first = time(NULL);
                     system(run_syntax);
-                    second = time(NULL);
                 
                 
                     dup2(fd_backup, 1);
                     dup2(fd_backup2,2);
-                    printf("wait time is %f seconds\n\n", difftime(second, first));
                     close(stdoutfile_fd); 
                     close(error_fd);
             
@@ -370,10 +348,10 @@ void program_autocompile(char* directory_path_std, char* directory_path_ans, int
     system("killall 13.exe");
 
     
-     //error option process
+     //error 옵션 작동 과정
      if(error_flag == 1) {
-        sprintf(remove_error_directory,"%s %s","rm -rf",e_filename); // because remove all files and sub directory
-        sprintf(make_error_directory,"%s %s","mkdir",e_filename); // because remove all files and sub directory
+        sprintf(remove_error_directory,"%s %s","rm -rf",e_filename); // 모든 서브 디렉터리를 지우기 위한 구문
+        sprintf(make_error_directory,"%s %s","mkdir",e_filename); // 디렉터리를 새로 생성하기 위한 구문
 
         if((count = scandir(".", &namelist, NULL, alphasort)) == -1) {
         fprintf(stderr, "%s Directory Scan Error: %s\n", "ssu_score", strerror(errno));
@@ -382,7 +360,7 @@ void program_autocompile(char* directory_path_std, char* directory_path_ans, int
 
         for(idx = 0; idx < count; idx++) {
             if(strcmp(namelist[idx] -> d_name, e_filename) == 0 )
-                system(remove_error_directory); // because remove all files and sub directory
+                system(remove_error_directory); // 만약 존재하는 디렉터리면 지우고 다시 만들기 위함
         }
 
         if((std_count = scandir("STD_DIR/", &stdnamelist, NULL, alphasort)) == -1) {
@@ -646,8 +624,9 @@ void blank_problem_check(char* directory_path_std, char* directory_path_ans) {
                 if(strcmp(buf_ANS, buf_STD) == 0)
                     continue;
 
-                else    
+                else {   
                     total_score_tab_for[std_idx].score[ans_idx] = ERROR;
+                }
 
                 //printf("%s :", std_filepathname[ans_idx][std_idx]);
                 //printf("%.2lf\n",total_score_tab_for[std_idx].score[cfile_index]);
--- ssu_score/ssu_score.h
+++ ssu_score/ssu_score.h
@@ -11,22 +11,22 @@
 #include <sys/stat.h>
 #include <sys/time.h>
 
-#define BUFFER_SIZE 2048
-#define WARNING 0.1
-#define ERROR 0
-#define LONG_PROCESS 0
+#define BUFFER_SIZE 2048 //버퍼 사이즈
+#define WARNING 0.1 // WARNING 발생시 0.1점 감점
+#define ERROR 0 //ERROR 발견시 0점 부여성
+#define LONG_PROCESS 0 //프로그램 수행시간 5초 초과 시 0점 부여 
 
-struct ssu_score_table_format{
-    char name[20];
-    char comma;
-    double score;
-    char linejump;
+struct ssu_score_table_format{ //ssu_score.csv 표 형태
+    char name[20]; //문제번호
+    char comma; // "," csv 파일에서 콤마로 구분함
+    double score; //score
+    char linejump; // 개행문자
 } ;
 
-struct total_score_table_format{
-    char student[20];
-    double score[49];
-    double sum;
+struct total_score_table_format{ //score.csv 포맷
+    char student[20]; //학번
+    double score[49]; //학생이 얻은 점수
+    double sum; // 총점
 } ;
 
  DIR *dir__ans, *sub_ans, *dir_std, *sub_std; //dir_info : 처음 부모디렉터리, dir_* : DIR_* 내부의 디렉터리
@@ -35,29 +35,29 @@ struct total_score_table_format{
    char dir_root_std[30] = "STD_DIR/"; //STD_DIR
    char dir_path_ans[30] = "\0"; //ANS_DIR  
    char dir_path_std[30] = "\0"; //STD_DIR
-   char dir_path_backup_ans[30] = "\0"; // ANS_DIR/SUBDIR/로 돌아감
-   char dir_path_backup_std[30] = "\0"; // STD_DIR/SUBDIR/로 돌아감
-   char sub_path_ans_backup[30] = "\0";
-   char sub_path_std_backup[30] = "\0";
-   char child_filename[30] = "\0";
+   char dir_path_backup_ans[30] = "\0"; // ANS_DIR/로 돌아감
+   char dir_path_backup_std[30] = "\0"; // STD_DIR/로 돌아감
+   char sub_path_ans_backup[30] = "\0"; // ANS_DIR/SUBDIR/로 돌아감
+   char sub_path_std_backup[30] = "\0"; // STD_DIR/SUBDIR/로 돌아감
    char buf_ANS[BUFFER_SIZE]; // ANS 의 파일내용을 담기 위한 변수
    char buf_STD[BUFFER_SIZE]; // STD의 파일내용을 담기 위한 변수
-   char buf_dirname[BUFFER_SIZE]; //bufarray for save directory_name
+   char buf_ERR[BUFFER_SIZE]; // ERROR의 내용을 담기 위한 변수
+   char buf_dirname[BUFFER_SIZE]; //디렉터리 이름을 담을 변수
    int fd_ans, fd_std; //fd : 파일 디스크립터, count : 읽은 buf의 갯수
-   int pro_count = 0;
+   int pro_count = 0; //총 문제 수
    struct timeval begin_t, end_t; //시작시간과 끝나는 시간	
-   struct ssu_score_table_format ssu_score_tab_for[49]; //ssu_score_table format
-   struct total_score_table_format total_score_tab_for[20]; //total_score_table format
-   time_t first, second;
+   struct ssu_score_table_format ssu_score_tab_for[49]; //ssu_score.csv 테이블에 대한 구조체
+   struct total_score_table_format total_score_tab_for[20]; //score.csv에 대한 구조체
+   time_t first, second; //파일 실행시간 측정
 
 
 
-void blank_problem_check(char *, char *);
-void program_autocompile(char*, char*, int, int, char*, char*);
-void program_problem_check(char *, char *);
-void ssu_score_table_create(char *);
-void score_table_create_proto(char *, char *);
-void score_table_create(char *, char *, int);
+void blank_problem_check(char *, char *); //빈칸 채우기 문제 채점
+void program_autocompile(char*, char*, int, int, char*, char*); //프로그래밍 문제 자동 컴파일
+void program_problem_check(char *, char *); //프로그래밍 문제 채점
+void ssu_score_table_create(char *); // ssu_score.csv 파일 생성하기
+void score_table_create_proto(char *, char *); //채점하기 이전의 score.csv를 만든다.
+void score_table_create(char *, char *, int); //채점 후의 최종 score.csv로 재생
 
 
 
